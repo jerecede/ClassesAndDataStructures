41db8c16b5938ba339e101d335089b94a81fa7a3
jeremiascedeno <80321905+jeremiascedeno@users.noreply.github.com>
2025-05-07 20:12:14 +0200

    delete, shift, unshift

--- ClassesAndDataStructures/Program.cs
+++ ClassesAndDataStructures/Program.cs
@@ -11,13 +11,13 @@ namespace ClassesAndDataStructures
             Student student3 = new("paperino", "de paperoni", 2000);
             Student student4 = new("topolino", "mouse", 2000);
 
-            Teacher teacher1 = new Teacher{ Name = "pico", Surname = "de pippis", Subject = "math" };
+            Teacher teacher1 = new Teacher { Name = "pico", Surname = "de pippis", Subject = "math" };
             Teacher teacher2 = new Teacher { Name = "archimede", Surname = "pitagorico" };
-            Teacher teacher3 = new Teacher{ Name = "zapotec" }; //name è required, il resto diventa null
+            Teacher teacher3 = new Teacher { Name = "zapotec" }; //name è required, il resto diventa null
 
             Student[] students = new Student[3]; //statico
             students[0] = student1;
-            Student[] newArray = (Student[])students.Append(student2);
+            //Student[] newArray = (Student[])students.Append(student2); //c'è un errore che non capisco
             students[2] = student3;
             Student[] newArray2 = AppendToStudentsArray(students, student4);
 
@@ -47,7 +47,24 @@ namespace ClassesAndDataStructures
             SuperList<Student> superList = new SuperList<Student>();
             superList.Push(student1);
             superList.Push(student2);
-            //superList.Add(teacher1); da errore
+            superList.Push(student3);
+            superList.Push(student4);
+            //superList.Push(teacher1); da errore
+
+            for (int i = 0; i < superList.Length; i++)
+            {
+                Console.WriteLine(superList.Get(i).Name);
+            }
+
+            Console.WriteLine("");
+
+            superList.Shift();
+            superList.Pop();
+
+            for (int i = 0; i < superList.Length; i++)
+            {
+                Console.WriteLine(superList.Get(i).Name);
+            }
         }
 
         static Student[] AppendToStudentsArray(Student[] originalArray, Student newStudent)
--- ClassesAndDataStructures/SuperList.cs
+++ ClassesAndDataStructures/SuperList.cs
@@ -9,12 +9,23 @@ namespace ClassesAndDataStructures
     internal class SuperList<T>
     {
         private T[] _realArray;
+        public int Length => _realArray.Length;
 
         public SuperList()
         {
             _realArray = new T[0];
         }
 
+        public T Get(int index)
+        {
+            if(index < 0 || index >= _realArray.Length)
+            {
+                throw new IndexOutOfRangeException();
+            }
+            return _realArray[index];
+        }
+
+        //aggiunge un elemento alla fine
         public void Push(T item)
         {
             var originalLegth = _realArray.Length;
@@ -28,18 +39,14 @@ namespace ClassesAndDataStructures
             _realArray = newArray;
         }
 
-        public T Get(int index)
-        {
-            if(index < 0 || index >= _realArray.Length)
-            {
-                throw new IndexOutOfRangeException();
-            }
-            return _realArray[index];
-        }
-
-        //toglie l'ultimo e lo restituisce anche
+        //toglie l'ultimo e restituisce elemento rimosso
         public T Pop()
         {
+            if (_realArray.Length == 0)
+            {
+                throw new InvalidOperationException("Cannot shift from an empty list.");
+            }
+
             int originalLength = _realArray.Length;
             int newArrayLength = _realArray.Length - 1;
             var newArray = new T[newArrayLength];
@@ -51,6 +58,61 @@ namespace ClassesAndDataStructures
             }
             _realArray = newArray;
             return element;
+        }
+
+        //aggiunge un elemento all'inizio
+        public void Unshift(T item)
+        {
+            var originalLegth = _realArray.Length;
+            var newArray = new T[originalLegth + 1];
+            newArray[0] = item;
+            for (int i = 1; i < originalLegth; i++)
+            {
+                newArray[i] = _realArray[i - 1];
+            }
+
+            _realArray = newArray;
+        }
+
+        //toglie il primo e restituisce elemento rimosso
+        public T Shift()
+        {
+            if (_realArray.Length == 0)
+            {
+                throw new InvalidOperationException("Cannot shift from an empty list.");
+            }
+
+            int newArrayLength = _realArray.Length - 1;
+            var newArray = new T[newArrayLength];
+            T element = _realArray[0];
+
+            for (int i = 1; i < _realArray.Length; i++)
+            {
+                newArray[i - 1] = _realArray[i];
+            }
+
+            _realArray = newArray;
+            return element;
+        }
+
+        public void Delete(int index)
+        {
+            if (index < 0 || index >= _realArray.Length)
+            {
+                throw new IndexOutOfRangeException();
+            }
+
+            int originalLength = _realArray.Length;
+            var newArray = new T[originalLength - 1];
+            
+            newArray = _realArray.Where((_, i) => i != index).ToArray();
+
+            _realArray = newArray;
+        }
+
+        public void Clear()
+        {
+            _realArray = new T[0];
         }
     }
 }
